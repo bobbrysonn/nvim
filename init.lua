b1582f941758a95ff57f78840cbfb5eaa4146cbe
Bob Moriasi <official.bobmoriasi@gmail.com>
2024-10-17 00:54:27 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,26 @@
+require("config.lazy")
+
+local opt = vim.opt
+
+-- Clipboard
+opt.clipboard = "unnamedplus"
+
+-- Editing
+opt.history = 1000
+
+-- Indentation
+opt.autoindent = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smarttab = true
+opt.tabstop = 2
+
+-- User Interface
+opt.number = true
+opt.relativenumber = true
+
+-- Search
+opt.hlsearch = true
+opt.ignorecase = true
+opt.incsearch = true
+opt.smartcase = true
---
+++ lua/plugins/completions.lua
@@ -0,0 +1,62 @@
+Custom_borders = {
+	{ "╭", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "╮", "FloatBorder" },
+	{ "│", "FloatBorder" },
+	{ "╯", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "╰", "FloatBorder" },
+	{ "│", "FloatBorder" },
+}
+
+return {
+  {
+    "hrsh7th/cmp-nvim-lsp"
+  },
+  {
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+
+			-- Setup
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				window = {
+					completion = { border = Custom_borders },
+					documentation = {
+						border = Custom_borders,
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+					},
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "luasnip" }, -- For luasnip users.
+          { name = "nvim_lsp" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			-- Load friendly snippets
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+}
